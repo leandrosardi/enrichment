e154a6d7920065b0f4b7d88a68370d0a1af0e7c7
leandrosardi <leandro.sardi@expandedventure.com>
2022-11-29 18:09:33 -0300

    leandrosardi/leads#95

--- models/skeleton/order.rb
+++ models/skeleton/order.rb
@@ -2,7 +2,7 @@ module BlackStack
     module Enrichment
         class Order < Sequel::Model(:vrf_order)
             many_to_one :user, :class=>:'BlackStack::MySaaS::User', :key=>:id_user
-            many_to_one :export, :class=>:'Leads::FlExport', :key=>:id_export
+            many_to_one :export, :class=>:'BlackStack::LeadsExport', :key=>:id_export
         end # class Order
     end # Enrichment
 end # BlackStack
